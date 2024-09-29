57cf634bb5f9e33942a753f58eefecf3f2196a92
jiwook_han <jiwook.han.dev@gmail.com>
2024-09-29 22:43:33 +0900

    Init

---
+++ MessageIO/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MessageIO
+//
+//  Copyright (c) 2024 z-wook. All right reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
